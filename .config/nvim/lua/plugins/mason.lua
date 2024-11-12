1a5c0b099f1af158543be1190fedc1e63c1ce4a7
julian silberbauer <j.silberbauer@proton.de>
2024-11-12 17:49:35 +0100

    rework, mostly nvim

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,12 +1,36 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },    
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-        mason.setup()
-        mason_lspconfig.setup()
+	"williamboman/mason.nvim",
+
+	dependencies = { "williamboman/mason-lspconfig.nvim" },
+	config = function()
+
+        	local mason = require("mason")
+        	local mason_lspconfig = require("mason-lspconfig")
+
+	        -- enable mason and configure icons
+        	mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				}
+			}
+		})
+
+        	mason_lspconfig.setup({
+            	-- list of servers for mason to install
+            	ensure_installed = {
+                	"ts_ls",
+                	"volar",
+			"html",
+			"cssls",
+			"tailwindcss",
+			"emmet_ls",
+			"lua_ls",
+            },
+            -- auto-install configured servers (with lspconfig)
+            automatic_installation = false, -- not the same as ensure_installed
+        })
     end,
 }
